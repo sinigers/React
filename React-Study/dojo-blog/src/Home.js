d1693c3318975def875dfa897a25be360294ae85
sinigers <sinigers@yahoo.com>
2021-01-26 08:43:41 +0200

    add click events

--- React-Study/dojo-blog/src/Home.js
+++ React-Study/dojo-blog/src/Home.js
@@ -1,9 +1,20 @@
 const Home = () => {
+const handleClick= (e) =>{
+    console.log('hello, ninjas', e);
+}
+const handleClickAgain= (name, e) =>{
+    console.log('Hello '+name, e.target);
+}
+
     return ( 
         <div className="home">
             <h2>Home page</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={(e)=> handleClickAgain('Pesho', e)}>Click Me Again</button>
         </div>
      );
 }
  
 export default Home;
+
+//e event object
