3b1ad78243d948b31787245554a8a0aa0688a089
sinigers <sinigers@yahoo.com>
2021-01-30 11:36:40 +0200

    conditional LOADING

--- React-Study/dojo-blog/src/Home.js
+++ React-Study/dojo-blog/src/Home.js
@@ -2,22 +2,25 @@ import { useState, useEffect } from "react";
 import BlogList from "./blogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
-    useEffect(() => {
-        fetch("http://localhost:8000/blogs")
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                setBlogs(data) //zadava stage na blogs s informaciata ot data -jsona
-            });
-    }, []);
-    return (
-      <div className="home">
-        {blogs && <BlogList blogs={blogs} title="All blogs" />}
-      </div>
-    );
-}
+  useEffect(() => {
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data); //zadava stage na blogs s informaciata ot data -jsona
+        setIsPending(false); //predi dase zaredi zaiavkata ot servera shte pokazva loading
+      });
+  }, []);
+  return (
+    <div className="home">
+      {isPending && <div>Loading...</div>} 
+      {blogs && <BlogList blogs={blogs} title="All blogs" />}
+    </div>
+  );
+};
 
 export default Home;
