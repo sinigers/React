b5e18bf5cc2c70764d8e2b169768e3f8a6bd4dae
sinigers <sinigers@yahoo.com>
2021-01-30 10:10:27 +0200

    Fetching Data with useEffect from json server

--- React-Study/dojo-blog/src/Home.js
+++ React-Study/dojo-blog/src/Home.js
@@ -2,37 +2,21 @@ import { useState, useEffect } from "react";
 import BlogList from "./blogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
-
-    const [name, setName] = useState('mario'); // 
-
-    const handleDelite = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id); //ako blog id savpada s handleDelete id
-        setBlogs(newBlogs);
-    }
+    const [blogs, setBlogs] = useState(null);
 
     useEffect(() => {
-        console.log('use Effect');
-        console.log(name);
-    }, []); // [] izpulniava funkciata samo pri purvoto rendvane;
-    //[name] izpulniava funkciata samo pri promqna na name ot const [name, setName]=useState('mario');
-    // ako niama [] izpulniava funkciata pri vsiako rendvane;
-
-    return ( <
-        div className = "home" >
-        <
-        BlogList blogs = { blogs }
-        title = "All blogs"
-        handleDelite = { handleDelite }
-        /> <
-        button onClick = {
-            () => setName('Alex') } > Change name < /button> <
-        p > { name } < /p> <
-        /div>
+        fetch("http://localhost:8000/blogs")
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setBlogs(data) //zadava stage na blogs s informaciata ot data -jsona
+            });
+    }, []);
+    return (
+      <div className="home">
+        {blogs && <BlogList blogs={blogs} title="All blogs" />}
+      </div>
     );
 }
 
