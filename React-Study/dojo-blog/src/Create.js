8363421f7b016ba8dffa572d47571612026cc590
sinigers <sinigers@yahoo.com>
2021-01-30 21:00:55 +0200

    Making a POST Request

--- React-Study/dojo-blog/src/Create.js
+++ React-Study/dojo-blog/src/Create.js
@@ -4,9 +4,22 @@ const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
+  const [isPending, setIsPending] = useState(false);
+
   const handleSubmit = (e) => {
     e.preventDefault(); //predotvratiava refresh pri nariskane na butona
-    const blog={title, body, author};  //suzdava obekt s keys title, body, author
+    const blog = { title, body, author }; //suzdava obekt s keys title, body, author
+
+    setIsPending(true);
+
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(blog), //obrushta obekt w json string
+    }).then(() => {
+      console.log("new blog added");
+      setIsPending(false);
+    });
   };
   return (
     <div className="create">
@@ -30,7 +43,8 @@ const Create = () => {
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
-        <button>Add blog</button>
+       { !isPending && <button>Add blog</button>}
+       { isPending && <button disabled>Adding blogg ...</button>}
         <p>{title}</p>
         <p>{body}</p>
         <p>{author}</p>
