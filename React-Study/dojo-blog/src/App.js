ba58ff685419ed96bae351c4f0dd09cb478490c4
sinigers <sinigers@yahoo.com>
2021-01-30 15:49:50 +0200

    The React Router in app.js

--- React-Study/dojo-blog/src/App.js
+++ React-Study/dojo-blog/src/App.js
@@ -1,17 +1,22 @@
-
-import Navbar from './Navbar';
-import Home from './Home';
+import Navbar from "./Navbar";
+import Home from "./Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
-   
-    return (
-        <div className = "App" >
-            <Navbar />
-        <div className = "content" >
-        < Home /> 
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
         </div>
-        </div>
-    );
+      </div>
+    </Router>
+  );
 }
 
 export default App;
