18c46b9161d5eb2d664d2dcb6af51dd688edfa1e
sinigers <sinigers@yahoo.com>
2021-01-30 12:57:45 +0200

    Making a Custom Hook usefetch.js

--- React-Study/dojo-blog/src/Home.js
+++ React-Study/dojo-blog/src/Home.js
@@ -1,30 +1,12 @@
 import { useState, useEffect } from "react";
 import BlogList from "./blogList";
+import useFetch from "./usefetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
+  const { data: blogs, isPending, error } = useFetch(
+    "http://localhost:8000/blogs"
+  );
 
-  useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        if (!res.ok) {
-          throw Error("could not fetch the data for that resource"); //izpisva, kogato data ot servera ne suvpada sus zaiavkata
-        }
-
-        return res.json();
-      })
-      .then((data) => {
-        setBlogs(data); //zadava state na blogs s informaciata ot data -jsona
-        setIsPending(false); //predi dase zaredi zaiavkata ot servera shte pokazva loading
-        setError(null); // ako zaiavkata e OK err e Null
-      })
-      .catch((err) => {
-        setIsPending(false); // kogato ima err state na IsPending e false - ne se pokazva "loading"
-        setError(err.massage);
-      });
-  }, []);
   return (
     <div className="home">
       {error && <div>{error}</div>}
---
+++ React-Study/dojo-blog/src/usefetch.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(url)  // primer url - "http://localhost:8000/blogs"
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("could not fetch the data for that resource"); //izpisva, kogato data ot servera ne suvpada sus zaiavkata
+        }
+
+        return res.json();
+      })
+      .then((data) => {
+        setData(data); //zadava state na blogs s informaciata ot data -jsona
+        setIsPending(false); //predi dase zaredi zaiavkata ot servera shte pokazva loading
+        setError(null); // ako zaiavkata e OK err e Null
+      })
+      .catch((err) => {
+        setIsPending(false); // kogato ima err state na IsPending e false - ne se pokazva "loading"
+        setError(err.massage);
+      });
+  }, [url]); // pri vsiaka promqna na url shte proveriava i vrushta data, isPending ili error
+  return{data, isPending, error};
+};
+export default useFetch;
