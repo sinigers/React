c7505fe7f0b7c704e8169ae7cba1d498d73f5314
sinigers <sinigers@yahoo.com>
2021-01-30 20:07:27 +0200

    Submit Events

--- React-Study/dojo-blog/src/Create.js
+++ React-Study/dojo-blog/src/Create.js
@@ -1,20 +1,32 @@
 import { useState } from "react";
 
 const Create = () => {
-    const [title, setTitle]=useState("")
-    const [body, setBody]=useState("")
-    const [author, setAuthor]=useState("mario")
-
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("mario");
+  const handleSubmit = (e) => {
+    e.preventDefault(); //predotvratiava refresh pri nariskane na butona
+    const blog={title, body, author};  //suzdava obekt s keys title, body, author
+  };
   return (
     <div className="create">
       <h2>Add new content</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog title:</label>
-        <input type="text" required value={title} onChange={(e) => setTitle(e.target.value)}/>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
         <label>Blog body:</label>
-        <textarea required value={body} onChange={(e) => setBody(e.target.value)}/>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        />
         <label>Blog author:</label>
-        <select value={author}  onChange={(e) => setAuthor(e.target.value)}>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
